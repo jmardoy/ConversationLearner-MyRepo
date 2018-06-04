c906cd3e98e5b96ab1983dc501a160a476638e78
Minerva Enriquez (Brickred Systems LLC) <v-maenr@microsoft.com>
2018-06-04 11:14:18 -0700

    Adding POM for new app. And updating appveyor config.

---
+++ test/e2e/pages/newappedit.js
@@ -0,0 +1,9 @@
+module.exports = {
+    url: 'http://localhost:5050/home',
+    elements: {
+        newentity: {
+            selector: '//a[text()="Entities"]',
+            locateStrategy: 'xpath'
+        }
+        }
+}
