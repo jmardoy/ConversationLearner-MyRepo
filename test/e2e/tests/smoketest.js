e3473e01205aa519e83e96d9741f46f10b165809
Minerva Enriquez (Brickred Systems LLC) <v-maenr@microsoft.com>
2018-06-06 11:17:38 -0700

    Backing up changes to smoke test

--- test/e2e/tests/smoketest.js
+++ test/e2e/tests/smoketest.js
@@ -1,27 +1,71 @@
 module.exports = {
-    'Create a New App': function (browser) {
+    'Create a New Model': function (browser) {
         // Instanciate a Home page POM
-        var cl = browser.page.homepage();
-
+        var homepage = browser.page.homepage();
+        var uniqueid = Date.now() - 1527837000000;
+        var newappbtn = '//div[text()="New Model"]';
+        var appname = 'input[id*="TextField"]';
+        
         // Open application
-        cl.navigate();
+        homepage.navigate();
 
-        var newappbtn = '//div[text()="New App"]';
+        // Create a new Model
         browser.waitForElementVisible('body', 1000)
             .maximizeWindow()
+            .pause(2000)
+            .click('button[data-testid="apps-list-button-create-new"]')
+            .pause(2000)
+            .url(function (response){
+            // Waits for the modal to be visible        
+            browser.waitForElementVisible('div[class="cl-modal_header"]', 4000)
+            .setValue(appname, 'a-Wed-' + uniqueid )
+            .setValue(appname,  browser.Keys.ENTER )
+            });
+        
+        //Create a new entities
+        browser
+        .pause(4000)
+        .url(function (response){
+
+            // Click Entities Navigation tab
+            browser.click('a[href$="/entities"]')
+            .pause(6000)
+            // Click new entity button
+            .click('[data-testid="entities-button-create"]')
+            
+            // Enter name for entity
+            .setValue('[data-testid="entity-creator-input-name"]', "username")
+                    
+            // Select multi-value
+            .click('[data-testid="entity-creator-input-multivalue"]')
+              
+            // Select the submit button
+            .click('[data-testid="entity-creator-button-save"]')
+            console.log("Entity created...")
+        });
+        // Create an action
+        browser
+        .pause(4000)
+        .url(function (response){
+            // Click Entities Navigation tab
+            browser.click('a[href$="actions"]')
+            .pause(3000)
             .useXpath()
-            .waitForElementVisible(newappbtn, 3000, false)
-            .click(newappbtn)
+            .setValue('//*[data-testid="dropdown-action-type"]', browser.keys.TAB )
+            .setValue('//*[data-testid="dropdown-action-type"]', "Hello World!")
             .useCss()
-            .pause(2000);
+            // Click new entity button
+            //.click('button[data-testid="actions-button-create"]')
 
-        var n = Date.now() - 1527837000000;
-        cl.waitForElementVisible('@appname', 4000)
-            .setValue('@appname', 'My-AutoBot-' + n )
-            .setValue('@appname', browser.Keys.ENTER)
+            // Select the submit button
+            .click('div[class="cl-modal-buttons_primary"]')
+            .pause(3000)
+            console.log("Action created...")
+        });
 
-        browser.pause(4000)
-            .end();
-    }
+            // Ensure new row is added to entities list with new entity name
+            //.Assert('.ms-DetailsRow-cell').contains("username")
+            browser.end();
+        }
 };
 
